2f9ee10e95a59e1c83382faeec5d0fd606436606
Sergey Chernikov <Sergey _Chernikov@epam.com>
2020-04-23 14:10:13 +0600

    #Task 1 -> Core concepts

---
+++ src/movies/install.bat
@@ -0,0 +1,5 @@
+call npm install --save-dev react react-dom
+call npm install --save-dev babel-core
+call npm install webpack --global
+call npm install --save-dev babel-loader
+call npm install --save-dev babel-preset-es2015 babel-preset-react
