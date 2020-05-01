33c3ba8c894f1aa4371381e1f62fb991ffa00ce2
Sergey Chernikov <Sergey _Chernikov@epam.com>
2020-05-01 09:43:50 +0600

    #task - init react app with npx create react app

--- src/movies/install.bat
+++
@@ -1,5 +0,0 @@
-call npm install --save-dev react react-dom
-call npm install --save-dev babel-core
-call npm install webpack --global
-call npm install --save-dev babel-loader
-call npm install --save-dev babel-preset-es2015 babel-preset-react
